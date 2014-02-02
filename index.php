231ca35ce1bec233fa8305c04e5572d4a4c34dce
Oleksii S. <alexey@hl-web.biz>
2014-02-02 02:34:00 +0200

    Update index.php
    hidden_mode and minor bugfixes

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ set_time_limit(2); // 2 second for everything
 
 session_start();
 
+$hidden_mode = false;
+
 $base_http_host = 'google.com';  //set this to the url you want to scrape
 $base = 'http://' . $base_http_host;
 
@@ -12,7 +14,7 @@ $url = $base . $_SERVER['REQUEST_URI'];
 
 // static locate to orign host. 
 // If you whant to hide orign host, better delete next code
-if(preg_match('~\.(jpg|jpeg|gif|png|bmp|js|css|txt|jar|tgz|rar|zip|gz)$~i', $_SERVER['REQUEST_URI']))// If you whant to hide orign host, better delete next code
+if(preg_match('~\.(jpg|jpeg|gif|png|bmp|js|css|txt|jar|tgz|rar|zip|gz)$~i', $_SERVER['REQUEST_URI']) && $hidden_mode == false)// If you whant to hide orign host, better delete next code
 {
   header("Location: ".$url);
   exit;
@@ -28,28 +30,31 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
 curl_setopt($curl, CURLOPT_TIMEOUT, 2);
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1);
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
 	curl_setopt($curl, CURLOPT_POST, 1);
 	curl_setopt($curl, CURLOPT_POSTFIELDS, $_POST);
 }
 
 // proxy cookie to orign
-foreach($_COOKIE as $key=>$value){
+foreach($_COOKIE as $key=>$value)
+{
 	if(is_array($value)) continue; // no sub array
 	curl_setopt($curl, CURLOPT_COOKIE, $key."=".$value."; domain=.".$base_http_host." ; path=/");
 }
 
-$response = curl_exec ($curl);
-curl_close ($curl);
+$response = curl_exec($curl);
+curl_close($curl);
 
 list($header, $body) = explode("\r\n\r\n", str_replace("HTTP/1.1 100 Continue\r\n\r\n", '', $response), 2);
 
 $header = split(chr(10),$header);
 foreach($header as $key=>$value){
-	if(!preg_match("~^transfer-encoding~i",$value)) {
-		$value = str_replace(array($base, $base_http_host), array($mydomain,$_SERVER['HTTP_HOST']), $value);
-		header(trim($value));
-	}
+	
+	if(preg_match("~^transfer-encoding~i",$value) continue;
+	$value = trim(str_replace(array($base, $base_http_host), array($mydomain,$_SERVER['HTTP_HOST']), $value));
+	header($value);
+
 }
 
 print str_replace($base, $mydomain, $body);
